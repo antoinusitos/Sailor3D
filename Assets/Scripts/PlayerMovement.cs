ceabee62362326a1c9580e8ca7d7b8069a0608b3
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2020-02-28 15:29:22 +0100

    starting to prototype

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform       myMainCamera = null;
+
+    private const float     mySpeed = 5.0f;
+
+    private Transform       myModel = null;
+
+    private void Start()
+    {
+        myMainCamera = Camera.main.transform;
+        myModel = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.Z))
+        {
+            dir += myMainCamera.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir -= myMainCamera.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir += myMainCamera.right;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            dir -= myMainCamera.right;
+        }
+
+        transform.position += dir.normalized * mySpeed * Time.deltaTime;
+
+        myModel.LookAt(transform.position + dir.normalized);
+    }
+}
